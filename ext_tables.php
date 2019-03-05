275d27825eee3bfb8bb22f1e7d2145e5a55ad5bc
Ronald Wopereis <woepwoep@gmail.com>
2019-03-05 13:28:46 +0100

    FlexForm

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,5 @@
 <?php
 defined('TYPO3_MODE') or die ('Access denied.');
+
+// Allow editing of BE records on sysfolder pages
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_rsrq_domain_model_query");
