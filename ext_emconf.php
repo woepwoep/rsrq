64732ccc49cfaac548934057fa78310e18812cbb
Ronald Wopereis <woepwoep@gmail.com>
2019-03-05 07:46:57 +0100

    Extbase structure

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,16 +11,16 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Red-Seadog Raw Query',
-    'description' => 'This extension is a lightweight version of WFQBE for TYPO3 v9. Features: raw query, extbase code, fluid templates.',
+    'description' => 'This extension is a lightweight version of WFQBE for TYPO3 v9 LTS. Features: raw query, extbase code, fluid templates.',
     'category' => 'plugin',
-    'version' => '9.5.5',
+    'author' => 'Ronald Wopereis',
+    'author_company' => 'Red-Seadog',
+    'author_email' => 'woepwoep@gmail.com',
     'state' => 'alpha',
+    'clearcacheonload' => true,
+    'version' => '9.5.5',
     'uploadfolder' => false,
     'createDirs' => '',
-    'clearcacheonload' => true,
-    'author' => 'Ronald Wopereis',
-    'author_email' => 'woepwoep@gmail.com',
-    'author_company' => 'Red-Seadog',
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-7.2.99',
