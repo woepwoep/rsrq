194272c8b86400221d220771c1626ee3b6cce91b
Ronald Wopereis <woepwoep@gmail.com>
2019-03-05 11:25:43 +0100

    Plugin: Query

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,14 @@
 <?php
-defined('TYPO3_MODE') or die ('Access denied.');
+defined('TYPO3_MODE') || die('Access denied.');
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'RedSeadog.Rsrq',
+    'Query',
+    [
+        'Query' => 'list',
+    ],
+    // non-cacheable actions
+    [
+        'Query' => '',
+    ],
+);
