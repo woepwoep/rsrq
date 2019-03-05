b8b5ab84b4b15b0dea1cc880b33fa83db3bfa870
Ronald Wopereis <woepwoep@gmail.com>
2019-03-05 08:25:56 +0100

    Added Query table

---
+++ Classes/Domain/Repository/QueryRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RedSeadog\Rsrq\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * Class QueryRepository
+ *
+ * @package RedSeadog\Rsrq\Domain\Repository
+ */
+class QueryRepository extends Repository
+{
+
+}
